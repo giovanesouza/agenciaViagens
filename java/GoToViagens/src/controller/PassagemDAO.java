6cf5a355d515ad42ad5aa18cc7bd3a258d0da929
giovanesouza <developergiovanesouza@gmail.com>
2022-09-11 16:48:24 -0300

    Bug da edição/atualização de dados corrigido. CRUD 100% Funcional

--- java/GoToViagens/src/controller/PassagemDAO.java
+++ java/GoToViagens/src/controller/PassagemDAO.java
@@ -13,26 +13,21 @@ public class PassagemDAO {
 	Connection conn = null;
 	PreparedStatement pstm = null;
 
-	// EDITAR <=======
 	public void save(Passagem passagem) {
 
-		// os ? são os parâmetros que nós vamos adicionar na base de dados
-
-		// EDITAR <=======
 		String sql = "INSERT INTO passagem(QUANTIDADE, ID_DESTINO)" + " VALUES(?,?)";
 
 		try {
-			// Cria uma conexão com o banco
+
 			conn = Conexao.createConnectionToMySQL();
 
 			pstm = conn.prepareStatement(sql);
 
-			// EDITAR <======= SET TIPO + GET DA CLASSE SEM O DAO
 			pstm.setInt(1, passagem.getQuantidade());
 			pstm.setInt(2, passagem.getIdDestino());
 			
-			
 			pstm.execute();
+			System.out.println("Passagem cadastrada com sucesso!");
 
 		} catch (Exception e) {
 
@@ -40,7 +35,6 @@ public class PassagemDAO {
 			
 		} finally {
 
-			// Fecha as conexões
 
 			try {
 				if (pstm != null) {
@@ -59,7 +53,7 @@ public class PassagemDAO {
 		}
 	}
 
-	// EDITAR <=======
+
 	public void removeById(int id) {
 
 		String sql = "DELETE FROM passagem WHERE ID_PASSAGEM = ?";
@@ -69,10 +63,10 @@ public class PassagemDAO {
 
 			pstm = conn.prepareStatement(sql);
 
-			// TIPO DE VALOR REFERENTE AO CPF E 1 = QTD QUE SERÁ EXCLUÍDA
 			pstm.setInt(1, id);
 		
 			pstm.execute();
+			System.out.println("Passagem excluída com sucesso!");
 
 		} catch (Exception e) {
 
@@ -97,36 +91,30 @@ public class PassagemDAO {
 		}
 	}
 
-	// EDITAR <=======
 	public void update(Passagem passagem) {
 		
-		String sql = "UPDATE passgem SET QUANTIDADE = ?, ID_DESTINO = ?"
+		String sql = "UPDATE passagem SET QUANTIDADE = ?, ID_DESTINO = ?"
 		+ " WHERE ID_PASSAGEM = ?";
 
 		try {
-			// Cria uma conexão com o banco
+
 			conn = Conexao.createConnectionToMySQL();
 
-			// Cria um PreparedStatment, classe usada para executar a query
 			pstm = conn.prepareStatement(sql);
 			
-			// EDITAR <=======
 			pstm.setInt(1, passagem.getQuantidade());
 			pstm.setInt(2, passagem.getIdDestino());
 			
 
-			// CAMPO QUE SERÁ UTILIZADO PARA BUSCAR O CADASTRO
 			pstm.setInt(3, passagem.getIdPassagem());
 			
-			// Executa a sql para inserção dos dados
 			pstm.execute();
+			System.out.println("Passagem atualizada com sucesso!");
 
 		} catch (Exception e) {
 
 			e.printStackTrace();
 		} finally {
-
-			// Fecha as conexões
 
 			try {
 				if (pstm != null) {
@@ -145,14 +133,14 @@ public class PassagemDAO {
 		}
 	}
 	
-	// EDITAR <=======
+
 	public List<Passagem> getPassagens() {
 
 		String sql = "SELECT * FROM passagem";
 
 		List<Passagem> passagens = new ArrayList<Passagem>();
 
-		// Classe que vai recuperar os dados do banco de dados
+
 		ResultSet rset = null;
 
 		try {
@@ -162,19 +150,15 @@ public class PassagemDAO {
 
 			rset = pstm.executeQuery();
 
-			// Enquanto existir dados no banco de dados, faça
 			while (rset.next()) {
 
 				Passagem passagem = new Passagem();
 
-				// Recupera o id do banco e atribui ele ao objeto
 				passagem.setQuantidade(rset.getInt("QUANTIDADE"));
 
-				// Recupera o nome do banco e atribui ele ao objeto
 				passagem.setIdDestino(rset.getInt("ID_DESTINO"));
 
 				
-				// Adiciono o contato recuperado, a lista de contatos
 				passagens.add(passagem);
 			}
 		} catch (Exception e) {
@@ -208,7 +192,6 @@ public class PassagemDAO {
 	}
 
 	
-	// EDITAR <=======
 	public Passagem getPassagemById(int id) {
 
 		String sql = "SELECT * FROM passagem where ID_PASSAGEM = ?";
@@ -225,7 +208,7 @@ public class PassagemDAO {
 			rset.next();
 
 			passagem.setQuantidade(rset.getInt("QUANTIDADE"));
-			passagem.setIdPassagem(rset.getInt("ID_DESTINO"));
+			passagem.setIdDestino(rset.getInt("ID_DESTINO"));
 	
 
 		} catch (Exception e) {
