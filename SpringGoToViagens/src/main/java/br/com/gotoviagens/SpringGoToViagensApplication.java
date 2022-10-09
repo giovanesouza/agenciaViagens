0fc720e1fd657c93ecd491dc1588793655c4e215
giovanesouza <developergiovanesouza@gmail.com>
2022-10-09 15:25:40 -0300

    Criação das rotas e 'conexão' entre as páginas - 100% funcional

--- SpringGoToViagens/src/main/java/br/com/gotoviagens/SpringGoToViagensApplication.java
+++ SpringGoToViagens/src/main/java/br/com/gotoviagens/SpringGoToViagensApplication.java
@@ -1,13 +1,20 @@
 package br.com.gotoviagens;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+// O CÓDIGO ABAIXO CORRIGIU O BUG
+// "Failed to configure a DataSource: 'url' attribute is not specified and no embedded datasource could be configured."
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringGoToViagensApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGoToViagensApplication.class, args);
+		
+		System.out.println("Aplicação rodando...");
 	}
 
 }
---
+++ SpringGoToViagens/src/main/java/br/com/gotoviagens/controller/RotasController.java
@@ -0,0 +1,90 @@
+package br.com.gotoviagens.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RotasController {
+
+	// ROTAS PRINCIPAIS
+	
+	@GetMapping("/")
+	// INICIALIZA A APLICAÇÃO
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/promocoes")
+	// PÁGINA PROMOÇÕES
+	public ModelAndView promocoes() {
+		ModelAndView modelAndView = new ModelAndView("html/promocoes");
+		return modelAndView;
+	}
+	
+	@GetMapping("/destinos")
+	// PÁGINA DESTINOS
+	public ModelAndView destinos() {
+		ModelAndView modelAndView = new ModelAndView("html/destinos");
+		return modelAndView;
+	}
+	
+	@GetMapping("/contato")
+	// PÁGINA CONTATO E CLIQUE PARA FALAR CONOSCO (RODAPÉ)
+	public ModelAndView contato() {
+		ModelAndView modelAndView = new ModelAndView("html/contato");
+		return modelAndView;
+	}
+	
+	@GetMapping("/login")
+	// PÁGINA LOGIN
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView("html/login1");
+		return modelAndView;
+	}
+	
+
+	
+	// ROTAS RODAPÉ
+	
+	@GetMapping("/quemsomos")
+	// PÁGINA QUEM SOMOS
+	public ModelAndView quemsomos() {
+		ModelAndView modelAndView = new ModelAndView("html/quemsomos");
+		return modelAndView;
+	}
+	
+	@GetMapping("/centralajuda")
+	// PÁGINA CENTRAL DE AJUDA
+	public ModelAndView centralajuda() {
+		ModelAndView modelAndView = new ModelAndView("html/centralajuda");
+		return modelAndView;
+	}
+	
+	@GetMapping("/termoscondicoes")
+	// PÁGINA TERMOS E CONDIÇÕES
+	public ModelAndView termoscondicoes() {
+		ModelAndView modelAndView = new ModelAndView("html/termoscondicoes");
+		return modelAndView;
+	}
+	
+	@GetMapping("/duvidas")
+	// PÁGINA DÚVIDAS
+	public ModelAndView duvidas() {
+		ModelAndView modelAndView = new ModelAndView("html/duvidas");
+		return modelAndView;
+	}
+	
+	
+	@GetMapping("/trabalheconosco")
+	// PÁGINA TRABALHE CONOSCO
+	public ModelAndView trabalheconosco() {
+		ModelAndView modelAndView = new ModelAndView("html/trabalheconosco");
+		return modelAndView;
+	}
+	
+	
+	
+	
+	
+}
